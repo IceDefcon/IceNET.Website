9f2377c2ca4dc98f74ddf0928b506dfc99186642
IceDefcon <ice.defcon@yahoo.com>
2022-02-17 09:54:56 +0100

    Version 2.1.0

--- js/main.js
+++
@@ -1,5 +0,0 @@
- // var timeout = setTimeout("location.reload(true);",5000);
- // function resetTimeout() {
- //   clearTimeout(timeout);
- //  timeout = setTimeout("location.reload(true);",5000);
- // }
